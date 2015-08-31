26d96a48442594813b62c7c5c9a53849c214a784
DeanHuangTW <DeanHuang@via.com.tw>
2015-08-31 10:27:39 +0800

    Add calendarView and SelectedDayChange

--- src/com/example/androidcalendar/MainActivity.java
+++ src/com/example/androidcalendar/MainActivity.java
@@ -1,34 +1,45 @@
 package com.example.androidcalendar;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 
+	private TextView mChooseDay;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mChooseDay = (TextView)  findViewById(R.id.chooseDay);		
+		// TextView default string is today
+		Calendar mCalendar = Calendar.getInstance();
+		int year = mCalendar.get(Calendar.YEAR);
+		int month = mCalendar.get(Calendar.MONTH) + 1;
+		int day = mCalendar.get(Calendar.DAY_OF_MONTH);
+		showDate(year, month, day);
+		
+		// click date
+		CalendarView calendarView=(CalendarView) findViewById(R.id.calendarView);
+		calendarView.setOnDateChangeListener(new OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+            	// show choose day
+            	showDate(year, month  + 1, dayOfMonth);
+            }
+        });
+	}
+	
+	private void showDate(int year, int month, int day) {
+		mChooseDay.setText(String.valueOf(year) + "-" + 
+				String.valueOf(month) + "-" + 
+				String.valueOf(day));
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
